f7cac3f3b7710b14db5e1149f5a8cf2c04295bc7
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-15 22:46:55 +0100

    Header de la homepage
    - Variables CSS de base
    - Hero du header (titre et description)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
   return (
     <>
       <header>
-        <h1>Développeur Full-Stack en devenir</h1>
+        <div className="header__hero"></div>
+        <h1 className="header_title">Développeur Web en devenir</h1>
+        <p className="header_description">
+          Moi c’est <em>Maxime</em>, étudiant en informatique à La Rochelle
+          Université. Ancien barman passionné par l’art du service, je me
+          réinvente aujourd’hui dans le développement Web.
+        </p>
       </header>
     </>
   );
