74915ae4aa0823b513a2cf287b8f798093207f6c
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-03-15 21:24:06 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Développeur Full-Stack en devenir</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
